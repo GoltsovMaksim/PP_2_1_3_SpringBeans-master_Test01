15e786d9fa8b1f31a7dc2c2fef018ee805bd2dc2
Maksim Goltsov <mgoltsov@bk.ru>
2022-10-20 11:56:31 +0300

    Initial commit

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,8 @@
+package app.model;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component("dogBean")
+public class Dog extends Animal {
+}
